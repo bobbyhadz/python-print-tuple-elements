4f128d8165c6d386edbf33e0d78739e6216e700f
bobbyhadz <opmftw@gmail.com>
2024-04-09 13:57:38 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Print tuple elements in Python
+
+my_tuple = ('one', 'two', 'three')
+
+# ✅ print a tuple
+print(my_tuple)  # 👉️ ('one', 'two', 'three')
+
+# ✅ print the first element in a tuple
+print(my_tuple[0])  # 👉️ 'one'
+
+# ✅ print the last element in a tuple
+print(my_tuple[-1])  # 👉️ 'three'
+
+# ✅ print a slice of a tuple
+print(my_tuple[0:2])  # 👉️ ('one', 'two')
